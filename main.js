acb777c44db3f041d7b478530fb24a1e1a9847af
uzi <43224578+vickkie@users.noreply.github.com>
2024-03-24 01:11:36 +0300

    combine main js
    Co-Authored-By: Quinzel <159898936+uzitrake@users.noreply.github.com>

--- main.js
+++ main.js
@@ -1,33 +1,17 @@
-import { partytownSnippet } from "@builder.io/partytown/integration";
-// main.js
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker
-      .register("./offline-uzitrake.js")
-      //  .then(reg=>
-      //   // console.log('serviceWorker')
-      //   )
-      .catch((err) => console.log("Error:", err));
-  });
-}
+// import { partytownSnippet } from "@builder.io/partytown/integration";
+// // main.js
+// if ("serviceWorker" in navigator) {
+//   window.addEventListener("load", () => {
+//     navigator.serviceWorker
+//       .register("./offline-uzitrake.js")
+//       //  .then(reg=>
+//       //   // console.log('serviceWorker')
+//       //   )
+//       .catch((err) => console.log("Error:", err));
+//   });
+// }
 
-const snippetText = partytownSnippet();
-const el = document.createElement("script");
-el.innerText = snippetText;
-document.body.appendChild(el);
-
-//Group 2: updaTE TIME
-
-document.addEventListener("DOMContentLoaded", () => {
-  let time = document.querySelector(".time");
-  function updateTime() {
-    var date = new Date();
-    time.innerHTML = date.toTimeString().substr(0, 5);
-  }
-  updateTime();
-  setInterval(() => {
-    updateTime();
-  }, 1000);
-});
-
-//Group 3:
+// const snippetText = partytownSnippet();
+// const el = document.createElement("script");
+// el.innerText = snippetText;
+// document.body.appendChild(el);
