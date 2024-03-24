c7e3bc3416d174edb368e961de2c42f63fdb3f6b
vickkie <vickkietrake@gmail.com>
2024-03-24 15:06:52 +0300

    improv

--- js/preloader/utils.js
+++ js/preloader/utils.js
@@ -5,43 +5,24 @@
 const lerp = (a, b, n) => (1 - n) * a + n * b;
 
 // Gets the mouse position
-const getMousePos = e => {
-    return { 
-        x : e.clientX, 
-        y : e.clientY 
-    };
+const getMousePos = (e) => {
+  return {
+    x: e.clientX,
+    y: e.clientY,
+  };
 };
 
 const calcWinsize = () => {
-    return {width: window.innerWidth, height: window.innerHeight};
+  return { width: window.innerWidth, height: window.innerHeight };
 };
 
 const getRandomInteger = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
 
 // Preload images
-const preloadImages = (selector = 'img') => {
-    return new Promise((resolve) => {
-        imagesLoaded(document.querySelectorAll(selector), {background: true}, resolve);
-    });
+const preloadImages = (selector = "img") => {
+  return new Promise((resolve) => {
+    imagesLoaded(document.querySelectorAll(selector), { background: true }, resolve);
+  });
 };
 
-// Preload fonts
-const preloadFonts = (id) => {
-    return new Promise((resolve) => {
-        WebFont.load({
-            typekit: {
-                id: id
-            },
-            active: resolve
-        });
-    });
-};
-
-export {
-    lerp,
-    getMousePos,
-    calcWinsize,
-    getRandomInteger,
-    preloadImages,
-    preloadFonts
-};
+export { lerp, getMousePos, calcWinsize, getRandomInteger, preloadImages };
