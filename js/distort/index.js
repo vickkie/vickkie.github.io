daac7b0438f9b20e5881a1ae88d3028b8d5f628c
uzi <43224578+vickkie@users.noreply.github.com>
2024-03-26 11:53:39 +0300

    Update index.js

--- js/distort/index.js
+++ js/distort/index.js
@@ -1,31 +1,26 @@
-import { Cursor } from './cursor.js';
-import { MenuItem } from './menuItem.js';
-// Splitting (used to split the menu item texts to spans/characters)
-// import 'splitting/dist/splitting.css';
-// import 'splitting/dist/splitting-cells.css';
-// import Splitting from 'splitting';
+import { Cursor } from "./cursor.js";
+import { MenuItem } from "./menuItem.js";
 
 // initialize Splitting
 const splitting = Splitting();
 
 // initialize custom cursor
-const cursor = new Cursor(document.querySelector('.cursor'));
+const cursor = new Cursor(document.querySelector(".cursor"));
 
 // Menu Items
-[...document.querySelectorAll('.menu > a')].forEach(el => new MenuItem(el));
+[...document.querySelectorAll(".menu > a")].forEach((el) => new MenuItem(el));
 
 let togglePathx = document.querySelector(".dark-toggle");
 
-      togglePathx.addEventListener("click", () => {
-
-        [...document.querySelectorAll('a')].forEach(link => {
-           cursor.enter();
-           cursor.leave();
-         })
-      });
+togglePathx.addEventListener("click", () => {
+  [...document.querySelectorAll("a")].forEach((link) => {
+    cursor.enter();
+    cursor.leave();
+  });
+});
 
 // mouse effects on all links
-[...document.querySelectorAll('a')].forEach(link => {
-    link.addEventListener('mouseenter', () => cursor.enter());
-    link.addEventListener('mouseleave', () => cursor.leave());
+[...document.querySelectorAll("a")].forEach((link) => {
+  link.addEventListener("mouseenter", () => cursor.enter());
+  link.addEventListener("mouseleave", () => cursor.leave());
 });
